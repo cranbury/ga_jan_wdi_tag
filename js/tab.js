ccc9ce575831ef050972618d77e60ff2103bb935
Granger <GrangerAbuhoff@gmail.com>
2014-02-26 16:19:01 -0500

    remove extra js

--- js/tab.js
+++ js/tab.js
@@ -16,19 +16,11 @@ $(document).ready(function(){
 
   //
 
-  var Clock = function(){
+  var Day = function(){
 
     var $el = $("#clock");
 
-    function updateClock(){
-      var currentTime = new Date();
-      var currentHours = currentTime.getHours();
-      var currentMinutes = currentTime.getMinutes();
-      currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-
-      var week = 7;
-      var day = 3;
-      //Need to add functions to get week, day
+    function updateDay(){
 
       var today = new Date();
       var dd = today.getDate();
@@ -71,57 +63,16 @@ $(document).ready(function(){
 
     }
 
-    updateClock();
-    var intervalID = window.setInterval(updateClock, 10000);
+    updateDay();
+    var intervalID = window.setInterval(updateDay, 10000);
 
   };
 
   //
 
-  var Pairs = function(){
-    var desk = localStorage["desk"];
+  
 
-    if(!desk){
-      return;
-    }
 
-    function displayPairs(obj){
-      var html = "<p>Desk <strong>";
-      html += desk;
-      html += "</strong> &mdash; Day <strong>";
-      html += obj.day;
-      html += "</strong>";
+  Day();
 
-      if(obj.pairs[desk].length){
-        html += " &mdash; Students ";
-
-        html += obj.pairs[desk].map(function(student){
-          var sHtml = "<a href='https://github.com/";
-          sHtml += student.github + "'>";
-          sHtml += student.name + "</a>";
-
-          return sHtml;
-        }).join(" &amp; ");
-      }
-
-      html += "</p>";
-
-      $("#info").html(html);
-    }
-
-    if(localStorage[Key("day")]){
-      displayPairs(JSON.parse(localStorage[Key("day")]));
-    } else {
-
-      $.getJSON("http://aa-progress-tracker.herokuapp.com/api/pairs.json", function(data){
-        console.log(data);
-
-        localStorage[Key("day")] = JSON.stringify(data);
-        displayPairs(data);
-      });
-    }
-  };
-
-  Clock();
-  Pairs();
 });
